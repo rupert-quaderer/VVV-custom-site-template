b74399119d62ae33d5621a9e4b1c4a8176451ff8
Rupert Quaderer <ruepi@zuhause.li>
2019-05-08 09:02:11 +0200

    updates

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -22,37 +22,37 @@ touch ${VVV_PATH_TO_SITE}/log/nginx-error.log
 touch ${VVV_PATH_TO_SITE}/log/nginx-access.log
 
 if [ "${WP_TYPE}" != "none" ]; then
- # Install and configure the latest stable version of WordPress
-if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp/wp-load.php" ]]; then
-    echo "Downloading WordPress..."
-  noroot wp core download --version="${WP_VERSION}" --path="${VVV_PATH_TO_SITE}/public_html/wp"
-fi
+  # Install and configure the latest stable version of WordPress
+  if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp/wp-load.php" ]]; then
+    echo "Downloading WordPress..." 
+    noroot wp core download --version="${WP_VERSION}" --path="${VVV_PATH_TO_SITE}/public_html/wp"
+  fi
 
-if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp/wp-config.php" ]]; then
-  echo "Configuring WordPress Stable..."
-  noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+  if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp/wp-config.php" ]]; then
+    echo "Configuring WordPress Stable..."
+    noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
 define( 'WP_DEBUG', true );
 define( 'SCRIPT_DEBUG', true );
 PHP
-fi
-
-if ! $(noroot wp core is-installed); then
-  echo "Installing WordPress Stable..."
-
-  if [ "${WP_TYPE}" = "subdomain" ]; then
-    INSTALL_COMMAND="multisite-install --subdomains"
-  elif [ "${WP_TYPE}" = "subdirectory" ]; then
-    INSTALL_COMMAND="multisite-install"
-  else
-    INSTALL_COMMAND="install"
   fi
 
-  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
-else
-  echo "Updating WordPress Stable..."
-  cd ${VVV_PATH_TO_SITE}/public_html/wp
-  noroot wp core update --version="${WP_VERSION}" --path="${VVV_PATH_TO_SITE}/public_html/wp"
-fi
+  if ! $(noroot wp core is-installed); then
+    echo "Installing WordPress Stable..."
+
+    if [ "${WP_TYPE}" = "subdomain" ]; then
+      INSTALL_COMMAND="multisite-install --subdomains"
+    elif [ "${WP_TYPE}" = "subdirectory" ]; then
+      INSTALL_COMMAND="multisite-install"
+    else
+      INSTALL_COMMAND="install"
+    fi
+
+    noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
+  else
+    echo "Updating WordPress Stable..."
+    cd ${VVV_PATH_TO_SITE}/public_html/wp
+    noroot wp core update --version="${WP_VERSION}" --path="${VVV_PATH_TO_SITE}/public_html/wp"
+  fi
 fi
 cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 
